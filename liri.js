628ec90fa68b8b7ba55e1324cae08d75f46d82a6
JP <parker.tech9@gmail.com>
2019-05-01 13:15:48 -0400

    switched over input from only the first input after command to everything, it broke if you put spaces before basically

--- liri.js
+++ liri.js
@@ -1,26 +1,56 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var axios = require("axios");
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var moment = require('moment');
 var commandName = process.argv[2];
+var userSearch = process.argv.slice(3).join(" ");
 
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 function concertThis() {
-  var myUrl = "https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp";
-  
+  var myUrl = "https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp";
+
+  axios.get(myUrl)
+    .then(function(response) {
+      for(i = i; i < response.data.length; i++) {
+        var day = moment(response.data[i].datetime).format("dddd, MMMM Do YYYY, h:mm:ss a");
+        console.log("---------------------------------------");
+        console.log(`Venue Name: ${response.data[i].venue.name}`);
+        console.log(`Venue Location : ${response.data[i].venue.city}, ${response.data[i].venue.region}`)
+        console.log(`Event Date: ${day}`)
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 
 function spotifyThisSong() {
 
+  spotify.search({ type: 'track', query: userSearch }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  
+  for(i = 0; i < data.tracks.items.length; i++){
+    console.log("---------------------------------------");
+    console.log(`Artist: ${data.tracks.items[i].artists[0].name}`)
+    console.log(`Song Name: ${data.tracks.items[i].name}`)
+    console.log(`Album Name: ${data.tracks.items[i].album.name}`)
+    console.log(`Preview Link: ${data.tracks.items[i].preview_url}`)
+    }
+  });
 }
 
 function movieThis() {
-
+  var myUrl = 'http://www.omdbapi.com/?apikey=trilogy&t=' + userSearch
 }
 
 function doWhatItSays() {
-
+  console.log(process.argv)
+  console.log(userSearch)
 }
 
 switch (commandName) {
